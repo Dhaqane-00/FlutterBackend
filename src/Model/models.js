54edf263a0a2a84aa72e40078c4a3d0483421072
Abdilaahi_Dhaqane <Abdilaahimowliid@gmail.com>
2024-05-30 01:18:22 +0300

    new

--- src/Model/models.js
+++ src/Model/models.js
@@ -8,6 +8,7 @@ const userSchema = new Schema({
     password: String,
     photo: {
         type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
     role: {
         type: String,
