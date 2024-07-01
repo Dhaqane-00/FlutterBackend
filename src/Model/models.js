31f855cd0fb8976d3f96508d080e7022316142ae
Abdilaahi_Dhaqane <Abdilaahimowliid@gmail.com>
2024-07-01 13:08:00 +0300

    new

--- src/Model/models.js
+++ src/Model/models.js
@@ -24,7 +24,7 @@ userSchema.virtual("photoURL").get(function () {
     return this.photo;
   } else {
     return (
-      (process.env.IMAGE_URL || 'http://localhost:500/') +
+      (process.env.IMAGE_URL || 'https://flutterbackend-production-d4d0.up.railway.app/') +
       (this.photo ? this.photo : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
     );
   }
